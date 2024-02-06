5a8380806ca7036d4ae0232c06fff33995aaa7f3
Dist Bit <adp01@pm.me>
2024-02-07 00:55:44 +0300

    test

--- voice.sh
+++
@@ -1,9 +0,0 @@
-cd /home/pimania/dev/dictation
-
-touch ~/alex.txt
-# . ~/.env
-
-export OPENAI_API_KEY="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"
-echo "1" > ~/alex.txt
-python3 voice.py
-echo "2" > ~/alex.txt
